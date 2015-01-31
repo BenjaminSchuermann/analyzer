a08d3cc08d0fe1eca5bd64bfc0c466d540c8d010
Garog <garog@ruhrnuklear.de>
2015-01-31 18:40:16 +0100

    Wieder auf weiß geändert ;)

--- src/garog/view/MyButton.java
+++ src/garog/view/MyButton.java
@@ -15,7 +15,7 @@ public class MyButton extends JButton {
     public MyButton(String text) {
         super(text);
         this.setForeground(Color.BLACK);
-        this.setBackground(Color.BLUE);
+        this.setBackground(Color.WHITE);
         Border line = new LineBorder(Color.BLACK);
         Border margin = new EmptyBorder(5, 15, 5, 15);
         Border compound = new CompoundBorder(line, margin);
